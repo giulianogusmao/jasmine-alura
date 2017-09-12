6d89d2b60b9e5221fc475d755172f40709f05a51
Giuliano Gusmão <gusmaoggg@gmail.com>
2017-09-12 20:03:49 -0300

    convertendo class MaiorMenor para ES6

--- src/js/MaiorMenor.js
+++ src/js/MaiorMenor.js
@@ -1,28 +1,24 @@
-function MaiorMenor() {
-    let menor;
-    let maior;
-
-    let clazz = {
-        encontra(nums) {
-            try {
-                if (nums)
-                    [menor] = [maior] = nums;
-
-                nums.forEach(num => {
-                    if (num < menor) menor = num;
-                    if (num > maior) maior = num;
-                });
-            } catch (e) {
-                console.erro(e);
-            }
-        },
-        getMenor() {
-            return menor;
-        },
-        getMaior() {
-            return maior;
-        }
+class MaiorMenor {
+    constructor (menor, maior) {
+        this._menor = menor;
+        this._maior = maior;
     }
 
-    return clazz;
-};
+    encontra (nums) {
+        if (nums)
+            [this._menor] = [this._maior] = nums;
+
+        nums.forEach(num => {
+            if (num < this._menor) this._menor = num;
+            if (num > this._maior) this._maior = num;
+        });
+    }
+
+    getMenor () {
+        return this._menor;
+    }
+
+    getMaior () {
+        return this._maior;
+    }
+}
